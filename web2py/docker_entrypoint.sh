5fbc74a38d2e8deaf0b47c88df18d26f70902ce4
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-08-28 15:35:13 +0200

    Remove debug output

--- web2py/docker_entrypoint.sh
+++ web2py/docker_entrypoint.sh
@@ -8,7 +8,6 @@ sed -i s/"database_host =.*"/"database_host = $DATABASE_HOST"/  $APPCONFIG_FILE
 sed -i s/"database_user =.*"/"database_user = $DATABASE_USER"/  $APPCONFIG_FILE
 sed -i s/"database_name =.*"/"database_name = $DATABASE_NAME"/  $APPCONFIG_FILE
 sed -i s/"database_password =.*"/"database_password = $DATABASE_PASSWORD"/  $APPCONFIG_FILE
-cat $APPCONFIG_FILE
 
 echo "default_application = 'MoniTutor'" > /var/www/web2py/routes.py
 
