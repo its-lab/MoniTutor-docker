d421490be2dc43403a800a81fc65af0dc92f42a2
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-09-04 16:50:11 +0200

    Add icinga credentials to appconfig

--- web2py/docker_entrypoint.sh
+++ web2py/docker_entrypoint.sh
@@ -8,6 +8,9 @@ sed -i s/"database_host =.*"/"database_host = $DATABASE_HOST"/  $APPCONFIG_FILE
 sed -i s/"database_user =.*"/"database_user = $DATABASE_USER"/  $APPCONFIG_FILE
 sed -i s/"database_name =.*"/"database_name = $DATABASE_NAME"/  $APPCONFIG_FILE
 sed -i s/"database_password =.*"/"database_password = $DATABASE_PASSWORD"/  $APPCONFIG_FILE
+sed -i s/"icinga2_database_name =.*"/"icinga2_database_name = $ICINGA2_DATABASE_NAME"/  $APPCONFIG_FILE
+sed -i s/"icinga2_api_user =.*"/"icinga2_api_user = $ICINGA2_API_USERNAME"/  $APPCONFIG_FILE
+sed -i s/"icinga2_api_password =.*"/"icinga2_api_password = $ICINGA2_API_PASSWORD"/  $APPCONFIG_FILE
 
 echo "default_application = 'MoniTutor'" > /var/www/web2py/routes.py
 
